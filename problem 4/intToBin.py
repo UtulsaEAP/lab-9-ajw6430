5daeffc186d76f030f3c34831045584d701e2a39
ajw6430 <ajw6430@utulsa.edu>
2024-10-28 21:13:33 -0500

    2, 3, and 4 done, not sure about 1

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -1,17 +1,19 @@
 def int_to_reverse_binary(num1):
-    binary_val = ''
-#write your while loop here
-    #while num1 > 0:
-        #write your code
-
+    x = num1
+    sepRevBin = []
+    if num1 == 0:
+        binary_val = '0'
+    else:
+        while x >= 1:
+            digit = x%2
+            sepRevBin.append(str(digit))
+            x = x//2
+        binary_val = ''.join(sepRevBin)
     return binary_val;
 
 
 def string_reverse(input_string): 
-    reverse_input = ''
-    
-   #write your for loop here
-    
+    reverse_input = input_string[::-1]
     return reverse_input
 
 if __name__ == '__main__':
